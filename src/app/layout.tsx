09e74797d5f617673090c89446de1e5dc40d56c4
mariusrp <marius.e.phillips@gmai.com>
2023-04-25 12:43:00 +0200

    Created the header section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,17 @@
+import Header from '@/components/Header'
 import './globals.css'
 
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      
+
       <body>
+        {/* {Header} */}
+        <Header/>
+        {/* {Navbar} */}
+        {/* {SearchBox} */}
+
         {children}
       </body>
     </html>
